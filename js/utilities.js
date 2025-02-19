2836492ae712d84fecab8c7e185ed5305e70be6d
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2025-02-19 04:36:49 +0000

    Finished all the functionality for the project.

---
+++ js/utilities.js
@@ -0,0 +1,87 @@
+// ChatGPT - Please generate 200 unique female names as an array in javascript
+const femaleNames = [
+    "Ava", "Sophia", "Isabella", "Mia", "Amelia", "Harper", "Evelyn", "Abigail", "Ella", "Chloe",
+    "Grace", "Lily", "Aria", "Zoe", "Charlotte", "Emily", "Mila", "Avery", "Scarlett", "Victoria",
+    "Madison", "Layla", "Luna", "Nora", "Hazel", "Riley", "Leah", "Audrey", "Samantha", "Camila",
+    "Skylar", "Brooklyn", "Bella", "Stella", "Maya", "Sofia", "Savannah", "Archer", "Camryn", "Zara",
+    "Nina", "Quinn", "Addison", "Ellie", "Hannah", "Autumn", "Kinsley", "Serenity", "Lucy", "Paisley",
+    "Riley", "Scarlett", "Violet", "Eleanor", "Lillian", "Kennedy", "Sarah", "Alice", "Aurora", "Ivy",
+    "Everly", "Madeline", "Claire", "Julia", "Katherine", "Natalie", "Sadie", "Gianna", "Paige", "Emery",
+    "Delilah", "Lucy", "Maria", "Gianna", "Jade", "Nova", "Eloise", "Rory", "Willow", "Piper",
+    "Zoey", "Addison", "Aubrey", "Sadie", "Cora", "Hannah", "Stella", "Eliza", "Alexa", "Maya",
+    "Mackenzie", "Jasmine", "Ariana", "Isla", "Maddison", "Camilla", "Harper", "Sophia", "Leah",
+    "Lydia", "Ivy", "Alice", "Aurora", "Hailey", "Ariana", "Eva", "Lilliana", "Norah", "Jordan",
+    "Lola", "Sloane", "Tessa", "Vivian", "Finley", "Genevieve", "Zara", "Lacey", "Nellie", "Maddie",
+    "Callie", "Lana", "Bridget", "Jenna", "Gemma", "Caroline", "Sienna", "Cleo", "Freya", "Penny",
+    "Vera", "Mia", "Miriam", "Livia", "Phoebe", "Elise", "Fiona", "Nadia", "Thea", "Tina", "Bethany",
+    "Megan", "Margaret", "Erin", "Daphne", "Kara", "Summer", "Greta", "Holly", "Juliette", "Francesca",
+    "Marley", "Selena", "Kaitlyn", "Jocelyn", "Kimberly", "Josephine", "Brooklyn", "Dakota", "Brianna",
+    "Tessa", "Felicity", "Esther", "Leila", "Phoebe", "Hazel", "Sasha", "Poppy", "Audrey", "Nina",
+    "Carmen", "Vanessa", "Marissa", "Adele", "Emma", "Tina", "Riley", "Lola", "Kendra", "Ellie",
+    "Seraphina", "Josephine", "Lauren", "Roxanne", "Leanne", "Maggie", "June", "Eva", "Daisy", "Macy",
+    "Addison", "Raegan", "Vivienne", "Bria", "Macy", "Alina", "Natalie", "Jacqueline", "Eden", "Alina"
+  ];
+
+// ChatGPT - Please generate 200 unique male names as an array in javascript
+const maleNames = [
+    "Liam", "Noah", "Oliver", "Elijah", "James", "William", "Benjamin", "Lucas", "Henry", "Alexander",
+    "Sebastian", "Jack", "Owen", "Samuel", "Matthew", "Joseph", "David", "Carter", "Julian", "Leo",
+    "Isaac", "Gabriel", "Wyatt", "John", "Mason", "Ethan", "Daniel", "Michael", "Jackson", "Aiden",
+    "Luke", "Caleb", "Ryan", "Nathan", "Asher", "Joshua", "Andrew", "Dylan", "Isaiah", "Grayson",
+    "Zachary", "Eli", "Connor", "Samuel", "David", "Thomas", "Charles", "Christian", "Hunter", "Cameron",
+    "Robert", "Aaron", "Adrian", "Angel", "Nolan", "Henry", "Miles", "Lincoln", "Christopher", "Jordan",
+    "Ryan", "Evan", "Jeremiah", "Jaxon", "Maverick", "Dominic", "Jeremiah", "Max", "Jason", "Austin",
+    "Caden", "Myles", "Jaden", "Carter", "Sawyer", "Elliot", "Ezekiel", "Levi", "Xander", "Kai",
+    "Landon", "Adam", "Roman", "Kai", "Nicholas", "Beau", "Jesse", "Victor", "Brandon", "Zane",
+    "Gavin", "Tyler", "Mason", "Ryder", "Theo", "Ezra", "Cody", "Sebastian", "Kingston", "Blake",
+    "Miles", "Harrison", "Dominic", "Amos", "Brayden", "Grayson", "Vaughn", "Bryce", "Tristan", "Elliott",
+    "Maddox", "Dean", "Dawson", "Jasper", "Brandon", "Marco", "Travis", "Lorenzo", "Finn", "Seth",
+    "King", "Reed", "Frederick", "Bennett", "Simon", "George", "Julius", "Dante", "Calvin", "Nash",
+    "Hunter", "Xavier", "Colton", "Jaxson", "Kaden", "Jace", "Rylan", "Luka", "Dakota", "Lyle",
+    "Bryan", "Tobias", "Trenton", "Colby", "Beckett", "Graham", "Jonah", "Lennox", "Quentin", "Zayden",
+    "Kaius", "Finnley", "Cyrus", "Julius", "Alec", "Jared", "Gage", "Kieran", "Solomon", "Avery",
+    "Rafael", "Lyle", "Milo", "Brock", "Damien", "Hugh", "Wesley", "Jace", "Flynn", "Corbin",
+    "Dean", "Tanner", "Vince", "Raymond", "Kendall", "Collin", "Jackson", "Cole", "Zander", "Rocco",
+    "Rhett", "Wilder", "Clayton", "Derrick", "Tucker", "Arthur", "Zeke", "Walter", "Bryce", "Theo",
+    "Angelo", "Knox", "Brody", "Damian", "Graham", "Briggs", "Ashton", "Charlie", "Dakota", "Lorenzo",
+    "Maddox", "Maximus", "Evan", "Roman", "Kenny", "Mason", "Samson", "Hale", "Ivan", "Quinn",
+    "Beckham", "Noel", "Alfred", "Kellan", "Flynn", "Waylon", "Bryson", "Bodie", "Deacon", "Shane"
+  ];
+
+// ChatGPT - Please generate 200 last names as an array in javascript
+const lastNames = [
+    "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "García", "Rodríguez", "Wilson",
+    "Martínez", "Hernández", "Lopez", "González", "Perez", "Taylor", "Anderson", "Thomas", "Jackson", "White",
+    "Harris", "Martin", "Thompson", "Moore", "Clark", "Lee", "Walker", "Allen", "Young", "King",
+    "Wright", "Scott", "Torres", "Nguyen", "Hill", "Adams", "Baker", "Nelson", "Carter", "Mitchell",
+    "Perez", "Roberts", "Evans", "Gonzalez", "Cameron", "Sanchez", "Morris", "Bailey", "Graham", "Kelly",
+    "Howard", "Grant", "Hughes", "Green", "Cunningham", "Rogers", "Cook", "Murphy", "Rivera", "Cooper",
+    "Richardson", "Woods", "James", "Barnes", "Henderson", "Gomez", "Jordan", "Kelly", "Chapman", "Bennett",
+    "Reed", "Morgan", "Bailey", "Long", "Perry", "Russell", "Scott", "Bryant", "Jameson", "Alexander",
+    "Ramirez", "Wood", "Cole", "Harrison", "Ford", "Gibson", "Bishop", "Garza", "Curtis", "Hughes",
+    "Douglas", "Foster", "Gordon", "Simpson", "Warren", "Dixon", "Hayes", "Grant", "Mendoza", "George",
+    "Simmons", "Scott", "Graham", "Woods", "Sullivan", "Morris", "Hawkins", "Reynolds", "Burns", "Jenkins",
+    "Austin", "Coleman", "Diaz", "Ramsey", "Lane", "Sanders", "Kelly", "Matthews", "Patterson", "Peters",
+    "Curtis", "Duncan", "Barnett", "Perkins", "Shaw", "Mills", "Weaver", "Charles", "Edwards", "Henry",
+    "Owens", "Franklin", "McDonald", "Butler", "Fisher", "Hansen", "Garrison", "Snyder", "Grant", "Cross",
+    "Sparks", "Bauer", "Lynch", "Mason", "Freeman", "Kelley", "Curtis", "Ramirez", "Zimmerman", "Baldwin",
+    "Berry", "Burnett", "Walsh", "Murphy", "Chapman", "Chavez", "Barker", "Christian", "Chavez", "Black",
+    "Davidson", "Gibson", "Cross", "Glover", "Davidson", "Holt", "Keller", "Stewart", "Ross", "Morgan",
+    "Hunter", "Kennedy", "Campbell", "Simmons", "Hansen", "Black", "Meyer", "Diaz", "Patel", "Vasquez",
+    "Sullivan", "Griffin", "Lawson", "Schmidt", "Haynes", "Vargas", "Ramirez", "Craig", "Morrison", "Freeman",
+    "Craig", "Norris", "Serrano", "Ferguson", "King", "Floyd", "Stephens", "Schneider", "Walters", "Wells",
+    "Snyder", "Carson", "Woodard", "David", "Hicks", "James", "Douglas", "Reynolds", "Stevens", "Mathews",
+    "Jenkins", "Richards", "Wheeler", "Brock", "Carson", "Oliver", "Mann", "Dean", "Bryant", "Fox"
+  ];
+
+function getRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function getFullName() {
+    let firstNames = maleRadio.checked ? maleNames : femaleNames;
+    return `${getRandom(firstNames)} ${getRandom(lastNames)}`;
+}
+
+let maleRadio = document.getElementById('male');
+let femaleRadio = document.getElementById('female');
